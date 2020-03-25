ca0af14bc4c8c23427799fdb3eacb9dbcfa55b17
Ahn <anhpnnd@gmail.com>
2020-03-25 11:39:54 +0100

    upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
   // Indicates whether each individual test should be reported during the run
   verbose: true,
 };
