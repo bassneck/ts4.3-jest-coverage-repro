fe4600a9c0db2836a46544a8d71c8ba83ca2f58e
Ahn <anhpnnd@gmail.com>
2020-05-04 13:53:11 +0200

    chore: update instructions to use different presets

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
+  /**
+   * replace with preset: 'ts-jest/presets/js-with-ts' if you need to compile js/ts with ts-jest, see the list of presets
+   * here https://kulshekhar.github.io/ts-jest/user/config/
+   */
   preset: 'ts-jest',
   // Indicates whether each individual test should be reported during the run
   verbose: true,
