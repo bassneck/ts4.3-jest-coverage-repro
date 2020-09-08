eaaed7b544810850b9fea4cb5b2202e47afd35c9
Ahn <anhpnnd@gmail.com>
2020-09-08 23:30:59 +0200

    chore: upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,9 @@
 /** @typedef {import('ts-jest')} */
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  clearMocks: true,
-  coverageDirectory: "coverage",
   /**
    * replace with preset: 'ts-jest/presets/js-with-ts' if you need to compile js/ts with ts-jest, see the list of presets
    * here https://kulshekhar.github.io/ts-jest/user/config/
    */
   preset: 'ts-jest',
-  // Indicates whether each individual test should be reported during the run
-  verbose: true,
 };
