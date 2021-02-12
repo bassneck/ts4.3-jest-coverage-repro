4ad468824f81cc776536692513c2da656467ab82
Ahn <anhpnnd@gmail.com>
2021-02-12 23:44:43 +0100

    build(devs-dep): upgrade deps

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
-/** @typedef {import('ts-jest')} */
-/** @type {import('@jest/types').Config.InitialOptions} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   /**
    * replace with preset: 'ts-jest/presets/js-with-ts' if you need to compile js/ts with ts-jest, see the list of presets
